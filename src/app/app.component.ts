e23e84965e3df64ac59c448b68848e75eea46113
Hiago Petris <Hiago.Petris@ibm.com>
2022-03-07 09:11:51 -0300

    Test board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { PhotoBoardService } from './shared/components/photo-board/services/photo-board.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Photo } from './shared/components/photo-board/interfaces/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular testing';
-  public likes = 0;
+  public photos$: Observable<Photo[]>;
 
-  public like(): void {
-    this.likes++;
+  constructor(private service: PhotoBoardService) {}
+
+  // Bom colocar no onInit ao invés do construtor pois aqui é gerenciado pelo Angular
+  public ngOnInit(): void {
+    this.photos$ = this.service.getPhotos();
   }
 }
---
+++ src/app/shared/components/photo-board/photo-board.component.ts
@@ -0,0 +1,32 @@
+import { Photo } from './interfaces/photo';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-board',
+  templateUrl: './photo-board.component.html',
+  styleUrls: ['./photo-board.component.scss']
+})
+export class PhotoBoardComponent implements OnChanges {
+
+  @Input() public photos: Photo[];
+  public rows: any[][] = [];
+
+  // Sempre que uma input properties mudar
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.photos) {
+      this.rows = this.groupColumns(changes.photos.currentValue);
+    }
+  }
+
+  private groupColumns(photos: Photo[]): any[][] {
+    const newRows = [];
+    const step = 4;
+
+    for(let index = 0; index < photos.length; index += step) {
+      newRows.push(photos.slice(index, index+step));
+    }
+
+    return newRows;
+  }
+
+}
